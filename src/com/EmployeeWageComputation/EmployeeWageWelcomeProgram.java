48bcb83b567cc70cf02c6b23ae768147abd2e774
“PrasannaMohanta” <pmohanta53@gmail.com>
2023-04-11 17:56:50 +0530

    Added:welcome program

---
+++ src/com/EmployeeWageComputation/EmployeeWageWelcomeProgram.java
@@ -0,0 +1,15 @@
+package com.EmployeeWageComputation;
+
+
+public class EmployeeWageWelcomeProgram {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[]args){
+        /*
+         *printing welcome to EmployeeWage program
+         */
+        System.out.println("welcome to EmployeeWage program");
+    }
+}
